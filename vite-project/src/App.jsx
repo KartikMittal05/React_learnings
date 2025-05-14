93ea9d941f79425070ee684aedf9912d96dd84d1
Kartik Mittal <kartik.22b0121110@abes.ac.in>
2025-05-14 22:24:50 +0530

    learn Structure of react

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+import Chai from './Chai.jsx'
+function App() {
+  
+
+  return (
+    <>
+    <Chai />
+    <h1>Vite + React + Tailwind CSS</h1>
+    </>
+   
+  )
+}
+
+export default App
