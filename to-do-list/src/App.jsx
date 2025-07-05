6a1539a79eee8c264431bdfb94af051e5cd41a81
Kartik Mittal <kartik.22b0121110@abes.ac.in>
2025-07-05 21:17:54 +0530

    added bharat-clock

--- to-do-list/src/App.jsx
+++ to-do-list/src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import AppName from './components/AppName.jsx';
+import AddTodo from './components/AddTodo.jsx';
+import Todoitem1 from './components/Todoitem1.jsx';
+import Todoitem2 from './components/Todoitem2.jsx';
+import "./App.css";
 function App() {
-  const [count, setCount] = useState(0)
+  return <center className='todo-container'>
+    <AppName />
+    <AddTodo />
+    <div className="items-container">
+      <Todoitem1 />
+      <Todoitem2 />
+    </div>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  </center>
 }
-
-export default App
+export default App;
---
+++ to-do-list/src/components/Todoitem1.jsx
@@ -0,0 +1,23 @@
+function Todoitem1() {
+    
+    return (
+        <div class="container">
+
+            <div class="row kg-row">
+        <div class="col-4">
+          Buy Mik
+        </div>
+        <div class="col-4">
+         4/10/2023
+        </div>
+         <div class="col-2">
+          <button type="button" class="btn kg-button btn-danger">Delete</button>
+         </div>
+      </div>
+
+        </div>
+    );
+}
+
+
+export default Todoitem1;
